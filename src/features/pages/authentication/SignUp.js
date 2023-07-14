188016ac4b8efb1172c8f885f001cae4fecff5fb
AmanWorku <amexworku2014@gmail.com>
2023-07-14 13:28:20 +0300

    Refactor signup

--- src/features/pages/authentication/SignUp.js
+++ src/features/pages/authentication/SignUp.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaArrowLeft }  from 'react-icons/fa'
 
 const SignUp = () => {
   return (
@@ -12,7 +14,11 @@ const SignUp = () => {
        <h1 class="text-white text-4xl font-bold mb-8">Sign Up</h1>
         <form class="w-full max-w-lg p-12 border border-gray-300 bg-gray-100 rounded-lg bg-opacity-10">
         <div class="mb-4">
-            <label for="username" class="block mb-2 text-md font-bold text-white dark:text-white">Your username</label>
+            <label for="name" class="block mb-2 text-md font-bold text-white dark:text-white">Your Name</label>
+            <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primaryGreen focus:border-primaryGreen block w-full p-2.5" placeholder="Your name here" required />
+        </div>
+        <div class="mb-4">
+            <label for="username" class="block mb-2 text-md font-bold text-white dark:text-white">Your Username</label>
             <input type="text" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primaryGreen focus:border-primaryGreen block w-full p-2.5" placeholder="UserName00" required />
         </div>
         <div class="mb-4">
@@ -20,10 +26,15 @@ const SignUp = () => {
             <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primaryGreen focus:border-primaryGreen block w-full p-2.5" placeholder="name@gmail.com" required />
         </div>
         <div class="mb-4">
-            <label for="password" class="block mb-2 text-md font-bold text-white dark:text-white">Your password</label>
+            <label for="password" class="block mb-2 text-md font-bold text-white dark:text-white">Your Password</label>
             <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primaryGreen focus:border-primaryGreen block w-full p-2.5" placeholder="••••••••" required />
         </div>
+        <div class="mb-4">
+            <label for="confirm-password" class="block mb-2 text-md font-bold text-white dark:text-white">Confirm Your Password</label>
+            <input type="password" id="confirm-password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primaryGreen focus:border-primaryGreen block w-full p-2.5" placeholder="••••••••" required />
+        </div>
         <button type="submit" class="text-white bg-primaryGreen hover:bg-primaryGreenDark focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+        <Link to="/" class="text-primaryGreen font-medium py-2 flex items-center gap-2"><FaArrowLeft /> Go Back</Link>
         </form>
       </div>
     </div>

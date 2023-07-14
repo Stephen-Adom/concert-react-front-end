fc494911175a28f55cdc57557df551809da70326
AmanWorku <amexworku2014@gmail.com>
2023-07-14 13:37:11 +0300

    Finish display of sign in and sign up forms

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -15,7 +15,7 @@ const Welcome = () => {
         </div>
         <div className="flex space-x-4">
         <Link to="/signin">
-			<button type="button" class="focus:outline-none text-white bg-primaryGreen hover:bg-primaryGreenDark focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 h-[2.5rem] mr-2 mb-4">Sign In</button>
+			<button type="button" class="focus:outline-none text-white bg-primaryGreen hover:bg-primaryGreenDark font-medium rounded-lg text-sm px-5 py-2.5 h-[2.5rem] mr-2 mb-4">Sign In</button>
 		</Link>
 		<Link to="/signup">
 			<button type="button" class="text-white hover:text-white border border-primaryGreen hover:bg-primaryGreen font-medium rounded-lg text-sm px-5 py-2.5 h-[2.5rem] mr-2 mb-4 ">Sign Up</button>

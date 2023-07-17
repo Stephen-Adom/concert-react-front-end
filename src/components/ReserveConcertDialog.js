1f0c70d72a2c5b63d31750b79d776d7e1179c9df
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-17 10:57:04 +0000

    add reserve form dialog on concert details

---
+++ src/components/ReserveConcertDialog.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { TbCalendarCheck } from "react-icons/tb";
+
+const ReserveConcertDialog = ({ visible, setVisible }) => {
+	const [value, setValue] = useState(null);
+	const items = [
+		{ name: "Option 1", value: 1 },
+		{ name: "Option 2", value: 2 },
+		{ name: "Option 3", value: 3 },
+	];
+
+	const renderIcon = () => {
+		return <TbCalendarCheck className="mr-1" />;
+	};
+
+	const footerContent = () => {
+		return (
+			<div>
+				<Button
+					label="No"
+					icon="pi pi-times"
+					onClick={() => setVisible(false)}
+					className="p-button-text"
+				/>
+				<Button
+					label="Reserve"
+					icon={renderIcon}
+					className="border bg-primaryGreen border-primaryGreen hover:!bg-lime-600 hover:!border-lime-600"
+					onClick={() => setVisible(false)}
+					autoFocus
+				/>
+			</div>
+		);
+	};
+	return (
+		<Dialog
+			header="Make a Reservation"
+			visible={visible}
+			position="bottom"
+			style={{ width: "30vw" }}
+			onHide={setVisible}
+			footer={footerContent}
+			draggable={false}
+			resizable={false}
+		>
+			<div>
+				<form>
+					<div className="form-group">
+						<label
+							htmlFor="concert_name"
+							className="block mb-2 text-sm font-medium text-left text-gray-900"
+						>
+							Select Concert Hall / City / Time
+						</label>
+						<select
+							id="city"
+							className={`block w-full p-3 text-sm text-gray-900 border rounded-sm bg-gray-50`}
+							defaultValue="Choose a concert hall"
+						>
+							{/* <option selected>Choose a country</option> */}
+							<option value="US">United States</option>
+							<option value="CA">Canada</option>
+							<option value="FR">France</option>
+							<option value="DE">Germany</option>
+						</select>
+					</div>
+				</form>
+			</div>
+		</Dialog>
+	);
+};
+
+export default ReserveConcertDialog;
--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,4 @@ export { default as ConcertTableDetails } from "./ConcertTableDetails";
 export { default as BackButton } from "./BackButton";
 export { default as MenuButton } from "./MenuButton";
 export { default as ErrorMessage } from "./ErrorMessage";
+export { default as ReserveConcertDialog } from "./ReserveConcertDialog";

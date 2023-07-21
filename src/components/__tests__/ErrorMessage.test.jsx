b8996d76e50f275a30f5842539f290a56d9aac94
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-21 15:19:57 +0000

    Add test for ErrorMessage Component

---
+++ src/components/__tests__/ErrorMessage.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import ErrorMessage from "../ErrorMessage";
+
+const testError = {
+	username: {
+		message: "Must be at least 3 characters",
+	},
+};
+
+const field = "username";
+
+describe("Error Message", () => {
+	it("should render correctly", () => {
+		render(<ErrorMessage error={testError} field={field} />);
+
+		const alertMessage = screen.getByRole("alert");
+
+		expect(alertMessage).toBeInTheDocument();
+		expect(alertMessage.textContent).toEqual(testError[field].message);
+	});
+
+	it("should not display any error if no errors found", () => {
+		render(<ErrorMessage error={{}} field={field} />);
+
+		const alertMessage = screen.queryByRole("alert");
+
+		expect(alertMessage).toBeFalsy();
+	});
+});

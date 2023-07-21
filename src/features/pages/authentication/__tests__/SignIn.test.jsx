7a5c70e6d72b86e4e779c66514473d8896735bc4
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-21 12:02:13 +0000

    update user-event for testing to v14

--- src/features/pages/authentication/__tests__/SignIn.test.jsx
+++ src/features/pages/authentication/__tests__/SignIn.test.jsx
@@ -1,26 +1,89 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { useForm } from "react-hook-form";
 import SignIn from "../SignIn";
 
 const mockStore = configureMockStore();
 
 const initialState = {
-	currentUser: null,
-	error: null,
-	loading: false,
-	token: null,
+	auth: {
+		currentUser: null,
+		error: null,
+		loading: false,
+		token: null,
+	},
 };
 
 const store = mockStore(initialState);
 
-describe("Sign In Page", () => {
-	it("should render correctly", () => {
+function setup(jsx) {
+	return {
+		user: userEvent.setup(),
+		...render(jsx),
+	};
+}
+
+describe("Sign In", () => {
+	test("should render correctly", () => {
 		render(
 			<Provider store={store}>
-				<SignIn />
+				<BrowserRouter>
+					<SignIn />
+				</BrowserRouter>
 			</Provider>
 		);
-		expect(screen.getByText("Sign In")).toBeInTheDocument();
+
+		const heading = screen.getByRole("heading", {
+			name: "Sign In",
+			level: 1,
+		});
+
+		const usernameField = screen.getByRole("textbox", {
+			id: "username",
+		});
+
+		const passwordField = screen.getByRole("textbox", {
+			id: "password",
+		});
+
+		const submitButton = screen.getByRole("button", {
+			name: "Sign In",
+		});
+
+		const goBackLink = screen.getByRole("link", {
+			name: "Go Back",
+		});
+
+		expect(heading).toBeInTheDocument();
+
+		expect(usernameField).toBeInTheDocument();
+
+		expect(passwordField).toBeInTheDocument();
+
+		expect(submitButton).toBeInTheDocument();
+
+		expect(goBackLink).toBeInTheDocument();
+	});
+
+	test("form should be display error messages when fields are invalid", async () => {
+		render(
+			<Provider store={store}>
+				<BrowserRouter>
+					<SignIn />
+				</BrowserRouter>
+			</Provider>
+		);
+
+		const signInButton = screen.getByRole("button", {
+			name: "Sign In",
+		});
+
+		await userEvent.click(signInButton); // Use userEvent.click instead of fireEvent.click
+
+		const errorMessage = screen.getByText(/Enter your username/i);
+		expect(errorMessage).toBeInTheDocument();
 	});
 });

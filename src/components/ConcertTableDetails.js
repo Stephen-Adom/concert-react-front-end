387aa8d588355b6357fb095844b86dfaaddcb8fa
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-21 15:13:35 +0000

    Add test for displaying concert details

--- src/components/ConcertTableDetails.js
+++ src/components/ConcertTableDetails.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import { Table } from 'flowbite-react';
-import { ConcertTableDetailsProp } from '../model/model.types';
+import React from "react";
+import { Table } from "flowbite-react";
+import { ConcertTableDetailsProp } from "../model/model.types";
 
 const ConcertTableDetails = ({ concert }) => (
-  <Table className="!drop-shadow-none table-container !rounded-none">
-    <Table.Body className="divide-y">
-      <Table.Row className="bg-white ">
-        <Table.Cell className="whitespace-nowrap text-gray-900 font-semibold dark:text-white !rounded-none !py-3 text-xs">
-          Name of Artist
-        </Table.Cell>
-        <Table.Cell className="!rounded-none !py-3 text-right text-gray-900 font-semibold text-xs">
-          {concert.artist}
-        </Table.Cell>
-      </Table.Row>
+	<Table className="!drop-shadow-none table-container !rounded-none">
+		<Table.Body className="divide-y">
+			<Table.Row className="bg-white ">
+				<Table.Cell className="whitespace-nowrap text-gray-900 font-semibold dark:text-white !rounded-none !py-3 text-xs">
+					Name of Artist
+				</Table.Cell>
+				<Table.Cell
+					data-testid="artist"
+					className="!rounded-none !py-3 text-right text-gray-900 font-semibold text-xs"
+				>
+					{concert.artist}
+				</Table.Cell>
+			</Table.Row>
 
-      <Table.Row className="bg-white ">
-        <Table.Cell className="whitespace-nowrap font-semibold text-gray-900 dark:text-white !py-3 text-xs">
-          Name of Band
-        </Table.Cell>
-        <Table.Cell className="!py-3 text-xs text-right text-gray-900 font-semibold">
-          {concert.band}
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
+			<Table.Row className="bg-white ">
+				<Table.Cell className="whitespace-nowrap font-semibold text-gray-900 dark:text-white !py-3 text-xs">
+					Name of Band
+				</Table.Cell>
+				<Table.Cell
+					data-testid="band"
+					className="!py-3 text-xs text-right text-gray-900 font-semibold"
+				>
+					{concert.band}
+				</Table.Cell>
+			</Table.Row>
+		</Table.Body>
+	</Table>
 );
 
 ConcertTableDetails.propTypes = ConcertTableDetailsProp;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,8 +4,21 @@ import { rest } from "msw";
 export const handlers = [
 	rest.post("http://localhost:4000/api/v1/login", (req, res, ctx) => {
 		// Persist user's authentication in the session
-		sessionStorage.setItem("is-authenticated", "true");
+		return res(
+			// Respond with a 200 status code
+			ctx.status(200),
+			ctx.json({
+				authentication_token: "123456fdgdgdf",
+				user_id: 1,
+				name: "John Doe",
+				email: "john@mail.com",
+				role: "user",
+				username: "john",
+			})
+		);
+	}),
 
+	rest.post("http://localhost:4000/api/v1/register", (req, res, ctx) => {
 		return res(
 			// Respond with a 200 status code
 			ctx.status(200),
